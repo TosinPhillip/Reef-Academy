5d5f98ad630b98bd83c6920e294425b1e6ec9507
Tosin <oluyemitosin72@gmail.com>
2025-04-28 04:26:41 +0100

    Update mongo_db.py

--- db/mongo_db.py
+++ db/mongo_db.py
@@ -1,14 +1,14 @@
 #from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo import MongoClient
-import dotenv
+from dotenv import load_dotenv
 
 
 # Load environment variables
-dotenv.load_dotenv()
+load_dotenv()
 
 # Get the URI from the environment
-MONGO_URI = dotenv.get_key(dotenv_path=dotenv.find_dotenv(), key_to_get='MONGO_URI')
+MONGO_URI =  os.getenv("MONGO_URI")
 
 
 # Create the MongoClient
